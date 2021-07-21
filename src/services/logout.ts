1bf40b76476cea892ad1b9ead14bb7325606190b
Katanis <emiliskakanis@gmail.com>
2021-07-21 23:16:54 +0300

    created logout function

--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,5 +1,15 @@
-const logout = () => {
+import { API } from '~/constants';
+import getUrl from '../utils/getUrl';
+const logout = async () => {
+    const url = getUrl(API.Logout)
 
+    await fetch(url, {
+        headers: {
+            authorization:
+                `${localStorage.getItem('token')}`,
+        }
+    }).catch((error) => console.error(error.message))
+    localStorage.removeItem('token');
 };
 
 export default logout;
