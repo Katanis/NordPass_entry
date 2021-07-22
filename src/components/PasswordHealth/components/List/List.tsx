ad4f4ff618666e8e084803fbe082335c26d81e2f
Katanis <emiliskakanis@gmail.com>
2021-07-22 11:37:38 +0300

    updated passwords list

--- src/components/PasswordHealth/components/List/List.tsx
+++ src/components/PasswordHealth/components/List/List.tsx
@@ -1,80 +1,36 @@
-import { FC, useState } from "react";
-import { IItem } from "~/services/getUserItems";
-import ItemIcon from "./components/ItemIcon";
-import updateItem from "../../../../services/updateItem";
-import Modal from "react-modal";
+import './list-style.scss';
 
-import "./list-style.scss";
+import { Box, Typography } from '@material-ui/core';
+import { FC } from 'react';
+import { IItem } from '~/services/getUserItems';
+
+import ItemIcon from './components/ItemIcon';
+import UpdateModal from './components/UpdateModal';
 
 interface IList {
   items: Array<IItem>;
 }
 
-interface IUpdateModal {
-  item: IItem;
-}
-
-const UpdateModal: FC<IUpdateModal> = ({ item }) => {
-  const [showModal, setShowModal] = useState(false);
-  const [newPass, setNewPass] = useState("");
-
-  return (
-    <>
-      <button className="update" onClick={() => setShowModal(true)}>
-        Update Password
-      </button>
-      <Modal
-        className="modal"
-        isOpen={showModal}
-        onRequestClose={() => setShowModal(false)}
-        contentLabel="Example Modal"
-      >
-        <h1>Update Password</h1>
-        <input
-          placeholder="new password"
-          className="input"
-          value={newPass}
-          onChange={(event) => setNewPass(event.target.value)}
-        />
-        <div className="pt-12px text-center">
-          <button
-            className="button"
-            onClick={async () => {
-              await updateItem({
-                ...item,
-                password: newPass,
-              });
-
-              window.location.reload();
-            }}
-          >
-            Change
-          </button>
-          <button
-            className="button ml-12px"
-            onClick={() => {
-              setNewPass("");
-              setShowModal(false);
-            }}
-          >
-            Cancel
-          </button>
-        </div>
-      </Modal>
-    </>
-  );
-};
-
 const List: FC<IList> = ({ items }) => (
   <ul className="list">
     {items.map((item) => (
       <li className="item" key={item.title}>
-        <ItemIcon title={item.title} />
-        <div>
-          <div className="title">{item.title}</div>
-          <div className="description">{item.description}</div>
-        </div>
-        <UpdateModal item={item} />
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          width="100%"
+          padding="0 2rem"
+        >
+          <Box display="flex" flexDirection="row">
+            <ItemIcon title={item.title} />
+            <Box display="flex" flexDirection="column">
+              <Typography variant="h6">{item.title}</Typography>
+              <Typography variant="body1">{item.description}</Typography>
+            </Box>
+          </Box>
+          <UpdateModal item={item} />
+        </Box>
       </li>
     ))}
   </ul>
