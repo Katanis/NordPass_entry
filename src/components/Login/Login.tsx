be464fe2c3210ff0d5889a525f68aba3dc6c02d5
Katanis <emiliskakanis@gmail.com>
2021-07-22 09:40:18 +0300

    updated error message for login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -20,7 +20,8 @@ const Login = () => {
       await login(username, password);
       push(Routes.PasswordHealth);
     } catch (error) {
-      setErrorMessage(error.message);
+      console.error(error.message);
+      setErrorMessage('Login failed wrong user credentials');
     }
   };
 
